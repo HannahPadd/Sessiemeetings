316f87f41fc7e89c7654b5eccc4841b9758d5679
Lillie <57945088+LilliePad@users.noreply.github.com>
2020-10-26 16:47:57 +0100

    Heb de databasing opnieuw geimplenteerd
    Databasing is nu geimplementeerd met entity framework

---
+++ SessionMeetingsClassLibrary/Controllers/SessionsController.cs
@@ -0,0 +1,20 @@
+﻿using SessionMeetingsClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SessionMeetingsClassLibrary.Controllers {
+    class SessionsController {
+        
+        public void CreateDB()
+        {
+            using (var db = new SessionsContext())
+            {
+                var name = "test";
+                var newSession = new SessionsModel() { name = name };
+                db.Session.Add(newSession);
+                db.SaveChanges();
+            }
+        } 
+    }
+}
---
+++ SessionMeetingsClassLibrary/Models/SessionsModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SessionMeetingsClassLibrary.Models {
+    public class SessionsModel {
+        public string name;
+        public string description;
+        public string location;
+        public DateTime dateTime;
+
+    }
+
+    public class SessionsContext : DbContext {
+        public DbSet<SessionsModel> Session { get; set; }
+    }
+}
