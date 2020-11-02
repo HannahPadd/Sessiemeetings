5103b574252a13232acea5a2c3cc4e9d6e341eec
Dktje <55189645+123Dktje@users.noreply.github.com>
2020-11-02 16:27:11 +0100

    AddSession screen
    Sessie's kunnen toegevoegd worden met datepicker. Ik probeer deze nog om te zetten naar datepicker met tijd en plaats

---
+++ ClassLibrary/Models/SessionItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.Models
+{
+    class SessionItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
