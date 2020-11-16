80cf46143d25e08711e7fa9433447b5a68b320f0
Obada <ubadafatta@gmail.com>
2020-11-16 15:52:25 +0100

    Example formulier geimplementeerd

---
+++ Sessiemeetings/Data/EvaluationForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sessiemeetings.Data
+{
+    public class EvaluationForm
+    {
+        public string Field1 { get; set; }
+        public string Field2 { get; set; }
+        public string Field3 { get; set; }
+        public string Field4 { get; set; }
+
+    }
+}
