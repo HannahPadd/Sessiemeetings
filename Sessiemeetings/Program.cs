c00d5fb539c535033ee7919122889ab37f354a90
Lillie <57945088+LilliePad@users.noreply.github.com>
2020-11-25 21:54:37 +0100

    Begonnen met het bouwen van DbConnect
    Ben begonnen met het maken van de DbConnect class. Hier staat alle logica om dingen op te slaan en te verwijderen in de database.

--- Sessiemeetings/Program.cs
+++ Sessiemeetings/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ClassLibrary;
 
 namespace Sessiemeetings
 {
@@ -15,6 +16,7 @@ namespace Sessiemeetings
     {
         public static void Main(string[] args)
         {
+            DbConnection dbConnection = new DbConnection();
             CreateHostBuilder(args).Build().Run();
         }
 
