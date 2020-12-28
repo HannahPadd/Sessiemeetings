391599b50163043d2883d560052f2324b06f38e8
lucer66 <48240748+lucer66@users.noreply.github.com>
2020-12-28 13:02:24 +0100

    agenda_luc
    ----

---
+++ ClassLibrary/Models/Appointment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.Models
+{
+    public class Appointment
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string Text { get; set; }
+
+
+
+        public Appointment(DateTime Start, DateTime End, string Text)
+        {
+            this.Start = Start;
+            this.End = End;
+            this.Text = Text;
+
+        }
+    }
+}
