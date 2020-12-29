3280a47d5650cad43523d5dc8bd3c0d8ceb6e11d
Lillie <57945088+LilliePad@users.noreply.github.com>
2020-12-29 14:06:27 +0100

    Added toast notifications

--- Sessiemeetings/Startup.cs
+++ Sessiemeetings/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Sessiemeetings.Areas.Identity;
 using Sessiemeetings.Data;
+using ClassLibrary.Service;
 
 namespace Sessiemeetings
 {
@@ -40,6 +41,7 @@ namespace Sessiemeetings
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+            services.AddScoped<ToastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
